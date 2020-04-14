9b80570d7c94c71506c9fae0c5a951c8c144eb73
NavePnow <evan.wong.stu@gmail.com>
2020-04-14 16:57:15 +0800

    feat: add id index, drop hashtag colum, split input data and save to file

--- textclassifier.py
+++ textclassifier.py
@@ -1,16 +1,19 @@
 from textprocessor import TextProcessor
 import pandas as pd
+import numpy as np
 
 
 class TextClassifier:
 
-    def __init__(self, in_dir, dictionary_file, hashtag_file, train_file):
+    def __init__(self, in_dir, dictionary_file, hashtag_file, input_file):
         self.in_dir = in_dir
         self.hashtag = set()
         self.dictionary = {}
         self.dictionary_file = dictionary_file
         self.hashtag_file = hashtag_file
+        self.input_file = input_file
         self.train_file = train_file
+        self.valid_file = valid_file
 
 
     def load_raw_data(self):
@@ -19,34 +22,50 @@ class TextClassifier:
         textprocessor.load_dictioanry()
         textprocessor.load_hashtag()
         dat = pd.read_csv(
-            self.in_dir + '/' + self.train_file, header=None)
+            self.in_dir + '/' + self.input_file, header=None)
+
         dat.columns = ['tweet', 'hashtag']
-        # add each hashtag to the data as label
-        total = ['tweet', 'hashtag']
+        # n = len(dat)
+        # nlist = range(0,n)
+        dat['id'] = None
+        dat = dat[['id', 'tweet', 'hashtag']]
+
+        total = ['id', 'tweet', 'hashtag']
         total = total + list(textprocessor.hashtag)
         dat = dat.reindex(columns=list(total), fill_value=0)
-
-        # preprocessing
         dat['tweet'] = dat['tweet'].apply(textprocessor.cleanup)
         dat['tweet'] = dat['tweet'].apply(textprocessor.informal_norm)
+
         dat['hashtag'] = dat['hashtag'].apply(textprocessor.del_hashtag)
         dat = dat.drop(dat[dat['hashtag'].map(len) <
                            1].index).reset_index(drop=True)
+
         dat['tweet'] = dat['tweet'].apply(textprocessor.drop_tweet)
         dat = dat.drop(dat[dat['tweet'].map(len) <
                            1].index).reset_index(drop=True)
-        
-        # assign every hashtag to the label
+        n = len(dat)
+        nlist = range(0, n)
+        dat['id'] = nlist
+
+        # assign label
         for i in range(len(dat['hashtag'])):
             tmp_list = dat['hashtag'][i].split(",")
             for j in range(len(tmp_list)):
                 tmp_list[j] = tmp_list[j].replace(' ', '')
                 dat[tmp_list[j]][i] = 1
-        return dat
+        return dat.drop(columns=['hashtag'])
 
+        def split_data(self,data):
+            self.train_file, self.valid_file = np.split(
+                data.sample(frac=1), [int(.7*len(data))])
+        
+        def save_to_file(self):
+            self.train_file.to_csv(self.in_dir + '/' +
+                                   "train.csv", index=False, header=True)
+            self.valid_file.to_csv(self.in_dir + '/' +
+                                   "valid.csv", index=False, header=True)
+            
 
-        def create_input(self):
-            pass
 
 if __name__ == '__main__':
     textclassifier = TextClassifier(
